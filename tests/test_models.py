505a08a69cb4e9aea78d2750fb5ca244cd3cafa1
gpederzani <giulia.pederzani@protonmail.com>
2023-03-22 14:33:11 +0100

    Add test for daily_std function

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,3 +29,12 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_standard_deviation():
+    """Test that the standard deviation works correctly."""
+    from inflammation.models import daily_std
+
+    test_input = np.array([[1, 3, -7], [-2, 0, 1], [1, 0, 0]])
+    test_result = np.array([1.41, 1.41, 3.56])
+
+    npt.assert_almost_equal(daily_std(test_input), test_result, decimal=2)
